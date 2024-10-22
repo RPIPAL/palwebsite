22af1c4d6f3220d09d25661417074fe5c07b1110
Justin Zhang <justinzwork23@gmail.com>
2024-10-21 23:06:48 -0400

    changes

--- pal/src/components/Footer.tsx
+++ pal/src/components/Footer.tsx
@@ -5,14 +5,14 @@ import { FaSquareXTwitter, FaDiscord } from "react-icons/fa6";
 function Footer() {
   return (
     <>
-      <div className="h-28 mb-12 flex justify-around">
-        <div className="flex flex-col w-5/12 mx-auto">
-          <i>Philippine American League</i>
-          <i>Est. 1993</i>
-        </div>
-        <div className="w-1/2">
-          <b className="text-xl">Follow us:</b>
-          <div className="flex justify-evenly mt-4 text-4xl">
+      <div className="h-28 mb-12 flex justify-around bg-black">
+        <div className="flex flex-col w-full mx-auto text-xl lg:flex-row lg:justify-around m-2 lg:w-5/6 lg:items-center lg:text-2xl ">
+          <div className="flex flex-col items-center lg:flex-row lg:w-1/2 *:ml-4 lg:justify-center">
+            <i>Philippine American League</i>
+            <i>Est. 1993</i>
+          </div>
+
+          <div className="flex w-2/3 m-auto justify-evenly text-4xl lg:text-5xl lg:w-1/2">
             <FaInstagram />
             <FaDiscord />
             <FaTiktok />
